1c12a6db0a185e4407c4fbbdfdd1cb3c5edde53a
ardianys <ardianys@gmail.com>
2024-05-20 10:15:41 +0700

    kamera OK

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:async';
+import 'dart:io';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: CameraWidget(),
     );
   }
 }
@@ -47,20 +51,102 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            CameraWidget(),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
     );
+  }
+}
+
+class CameraWidget extends StatefulWidget {
+  @override
+  _CameraWidgetState createState() => _CameraWidgetState();
+}
+
+class _CameraWidgetState extends State<CameraWidget> {
+  CameraController? _controller;
+  Future<void>? _initializeControllerFuture;
+  File? _image;
+
+  @override
+  void initState() {
+    super.initState();
+    _initCamera();
+  }
+
+  // Step 3 & 4: Initialize CameraController and get available cameras
+  void _initCamera() async {
+    final cameras = await availableCameras();
+    _controller = CameraController(cameras.first, ResolutionPreset.medium);
+    _initializeControllerFuture = _controller!.initialize();
+  }
+
+  // Step 5: Implement a method to take a picture
+  Future<void> _takePicture() async {
+    try {
+      await _initializeControllerFuture;
+      final image = await _controller!.takePicture();
+      final imagePath = File(image.path);
+
+      setState(() {
+        _image = imagePath;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Absen Buah Hati')),
+      // Step 6: Update UI
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: FutureBuilder<void>(
+              future: _initializeControllerFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return Center(
+                    // Center the container
+                    child: Container(
+                      width: MediaQuery.of(context).size.width *
+                          0.4, // 50% of screen width
+                      height: MediaQuery.of(context).size.height *
+                          0.4, // 50% of screen height
+                      child:
+                          CameraPreview(_controller!), // CameraPreview widget
+                    ),
+                  );
+                } else {
+                  return Center(child: CircularProgressIndicator());
+                }
+              },
+            ),
+          ),
+          _image == null
+              ? Text('No image selected.')
+              : Container(
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  height: MediaQuery.of(context).size.height * 0.4,
+                  child: Image.file(_image!, fit: BoxFit.cover),
+                ),
+          FloatingActionButton(
+            onPressed: _takePicture,
+            tooltip: 'Take Picture',
+            child: Icon(Icons.camera),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // Step 7: Dispose the controller
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
   }
 }
